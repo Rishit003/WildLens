7d481adbf94b1df55140526f7d7a1ca693846d64
Rishit <rishitinc@gmail.com>
2024-11-23 20:41:57 +0530

    a

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ import instagramIcon from '../images/instagram-icon.png'
 const Footer = () => {
     return(
         <footer>
-            <div style = {{display: 'flex', justifyContent: 'space-between'}}>
+            <div style = {{display: 'flex', justifyContent: 'space-evenly'}}>
                 <a href="https://www.github.com/Rishit003/WildLens" target="_blank"><img src={githubIcon} style={{border: "2px solid white", borderRadius: "50%"}} height = "50 px" alt="github" /></a>
                 <a href="https://www.instagram.com/idk.rishit" target="_blank" ><img src={instagramIcon} style={{border: "3px solid white", borderRadius: "50%"}} height = "50 px" alt="instagram" /></a>
             </div>     
