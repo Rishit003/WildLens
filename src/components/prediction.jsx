79eb035d3a07c77ce170688fe6666c318559e0d5
Rishit <rishitinc@gmail.com>
2024-11-23 14:03:51 +0530

    API linked

--- src/components/prediction.jsx
+++ src/components/prediction.jsx
@@ -21,7 +21,7 @@ const Prediction = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/predict', {
+      const response = await fetch('https://wildlens-t1wk.onrender.com', {
         method: 'POST',
         body: formData,
       });
