e5932a76b0df84fde466ca5262572ae5c9658dc0
Rishit <rishitinc@gmail.com>
2024-11-23 13:58:17 +0530

    Image sources updated

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import githubIcon from '../images/github-icon.png'
+import portfolioIcon from '../images/portfolio-icon.png'
+import instagramIcon from '../images/instagram-icon.png'
 
 const Footer = () => {
     return(
         <footer>
             <div style = {{display: 'flex', justifyContent: 'space-between'}}>
-                <a href="https://www.github.com/Rishit003/WildLens" target="_blank"><img src="./src/images/github-icon.png" style={{border: "2px solid white", borderRadius: "50%"}} height = "50 px" alt="github" /></a>
-                <a href="https://portfolio-website-henna-two.vercel.app/" target="_blank"><img src="./src/images/portfolio-icon.png" style={{border: "2px solid white", borderRadius: "50%"}} height = "50 px" alt="portfolio" /></a>
-                <a href="https://www.instagram.com/idk.rishit" target="_blank" ><img src="./src/images/instagram-icon.png" style={{border: "3px solid white", borderRadius: "50%"}} height = "50 px" alt="instagram" /></a>
+                <a href="https://www.github.com/Rishit003/WildLens" target="_blank"><img src={githubIcon} style={{border: "2px solid white", borderRadius: "50%"}} height = "50 px" alt="github" /></a>
+                <a href="https://portfolio-website-henna-two.vercel.app/" target="_blank"><img src={portfolioIcon} style={{border: "2px solid white", borderRadius: "50%"}} height = "50 px" alt="portfolio" /></a>
+                <a href="https://www.instagram.com/idk.rishit" target="_blank" ><img src={instagramIcon} style={{border: "3px solid white", borderRadius: "50%"}} height = "50 px" alt="instagram" /></a>
             </div>     
             <p>&copy; 2024 WildLens | All rights reserved.</p>
         </footer>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import logo from '../images/logo.png'
+import search from '../images/search.png'
 
 const Logo = () => {
   return (
     <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-      <img src="./src/images/logo.png" height = "80px" alt="logo" />
+      <img src={logo} height = "80px" alt="logo" />
       <h1 style={{fontSize: '70px', color: 'white', fontWeight: 'bold', margin : '10px'}}>
-        Wild<img src="./src/images/search.png" height = "40px" alt="search" />Lens
+        Wild<img src={search} height = "40px" alt="search" />Lens
       </h1>
     </div>
   );
